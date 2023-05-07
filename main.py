6f826ee7a5f749193cf950a7301c6df36ae025cf
Marcílio Freitas <36551921+marcilio-freitas27@users.noreply.github.com>
2023-05-07 22:28:53 +0000

    Adicionando exemplos com mqtt e a api com Flask.

--- main.py
+++ main.py
@@ -1,25 +1,122 @@
-import paho.mqtt.client as mqtt
+# import paho.mqtt.client as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+# # The callback for when the client receives a CONNACK response from the server.
+# def on_connect(client, userdata, flags, rc):
+#     print("Connected with result code "+str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+#     # Subscribing in on_connect() means that if we lose the connection and
+#     # reconnect then subscriptions will be renewed.
+#     client.subscribe("$SYS/#")
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+# # The callback for when a PUBLISH message is received from the server.
+# def on_message(client, userdata, msg):
+#     print(msg.topic+" "+str(msg.payload))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+# client = mqtt.Client()
+# client.on_connect = on_connect
+# client.on_message = on_message
 
-client.connect("mqtt.eclipseprojects.io", 1883, 60)
+# client.connect("mqtt.eclipseprojects.io", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+# # Blocking call that processes network traffic, dispatches callbacks and
+# # handles reconnecting.
+# # Other loop*() functions are available that give a threaded interface and a
+# # manual interface.
+# client.loop_forever()
+
+# python 3.6
+
+# import random
+# import time
+
+# from paho.mqtt import client as mqtt_client
+
+
+# broker = 'broker.emqx.io'
+# port = 1883
+# topic = "python/mqtt"
+# # generate client ID with pub prefix randomly
+# client_id = f'python-mqtt-{random.randint(0, 1000)}'
+# # username = 'emqx'
+# # password = 'public'
+
+# def connect_mqtt():
+#     def on_connect(client, userdata, flags, rc):
+#         if rc == 0:
+#             print("Connected to MQTT Broker!")
+#         else:
+#             print("Failed to connect, return code %d\n", rc)
+
+#     client = mqtt_client.Client(client_id)
+#     # client.username_pw_set(username, password)
+#     client.on_connect = on_connect
+#     client.connect(broker, port)
+#     return client
+
+
+# def publish(client):
+#     msg_count = 0
+#     while True:
+#         time.sleep(1)
+#         msg = f"messages: {msg_count}"
+#         result = client.publish(topic, msg)
+#         # result: [0, 1]
+#         status = result[0]
+#         if status == 0:
+#             print(f"Send `{msg}` to topic `{topic}`")
+#         else:
+#             print(f"Failed to send message to topic {topic}")
+#         msg_count += 1
+
+
+# def run():
+#     client = connect_mqtt()
+#     client.loop_start()
+#     publish(client)
+
+
+# if __name__ == '__main__':
+#     run()
+
+from flask import Flask, request, jsonify
+from flask_mqtt import Mqtt
+
+app = Flask(__name__)
+
+app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = ''  # Set this item when you need to verify username and password
+app.config['MQTT_PASSWORD'] = ''  # Set this item when you need to verify username and password
+app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
+app.config['MQTT_TLS_ENABLED'] = False  # If your broker supports TLS, set it True
+topic = '/flask/mqtt'
+
+mqtt_client = Mqtt(app)
+
+
+@mqtt_client.on_connect()
+def handle_connect(client, userdata, flags, rc):
+   if rc == 0:
+       print('Connected successfully')
+       mqtt_client.subscribe(topic) # subscribe topic
+   else:
+       print('Bad connection. Code:', rc)
+
+
+@mqtt_client.on_message()
+def handle_mqtt_message(client, userdata, message):
+   data = dict(
+       topic=message.topic,
+       payload=message.payload.decode()
+  )
+   print('Received message on topic: {topic} with payload: {payload}'.format(**data))
+
+
+@app.route('/publish', methods=['POST'])
+def publish_message():
+   request_data = request.get_json()
+   publish_result = mqtt_client.publish(request_data['topic'], request_data['msg'])
+   return jsonify({'code': publish_result[0]})
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=5000)
